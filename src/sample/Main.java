ef5190915c05de4dd7615d558bb865a6d2da28ce
Max <maxexmachina@gmail.com>
2020-02-11 19:39:51 +0300

    Edit buttons almost working

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,25 +2,27 @@ package sample;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuButton;
+import javafx.scene.control.*;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCodeCombination;
-import javafx.scene.input.KeyCombination;
+import javafx.scene.input.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main extends Application {
+
+    private Clipboard systemClipboard = Clipboard.getSystemClipboard();
 
     @Override
     public void start(Stage primaryStage) {
@@ -49,7 +51,7 @@ public class Main extends Application {
             if (selectedFile != null) {
                 try {
                     String content = listToString(readAll(selectedFile.getPath()));
-                    tArea.textProperty().setValue(content);
+                    tArea.setText(content);
                     setStageTitle(selectedFile.getName(), primaryStage);
                 } catch (IOException e) {
                     System.out.println(e.getMessage());
@@ -78,13 +80,83 @@ public class Main extends Application {
 
         file.getItems().addAll(newFile, open, save);
 
+        MenuButton edit = new MenuButton("Edit");
+        edit.setStyle("-fx-focus-color: transparent;");
+        MenuItem cut = new MenuItem("Cut");
+        MenuItem copy = new MenuItem("Copy");
+        MenuItem paste = new MenuItem("Paste");
+        MenuItem delete = new MenuItem("Delete");
+
+        cut.setOnAction(event -> {
+            String text = tArea.getSelectedText();
+            ClipboardContent content = new ClipboardContent();
+            content.putString(text);
+            systemClipboard.setContent(content);
+
+            IndexRange range = tArea.getSelection();
+            String origText = tArea.getText();
+            String firstPart = origText.substring(0, range.getStart());
+            String lastPart = origText.substring(range.getEnd());
+            tArea.setText(firstPart + lastPart);
+            tArea.positionCaret(range.getStart());
+        });
+        cut.setAccelerator(new KeyCodeCombination(KeyCode.X, KeyCombination.CONTROL_DOWN));
+
+        copy.setOnAction(event -> {
+            String text = tArea.getSelectedText();
+            ClipboardContent content = new ClipboardContent();
+            content.putString(text);
+            systemClipboard.setContent(content);
+        });
+        copy.setAccelerator(new KeyCodeCombination(KeyCode.C, KeyCombination.CONTROL_DOWN));
+
+        paste.setOnAction(event -> {
+            String clipboardText = systemClipboard.getString();
+            IndexRange range = tArea.getSelection();
+
+            String origText = tArea.getText();
+            String firstPart = origText.substring(0, range.getStart());
+            String lastPart = origText.substring(range.getEnd());
+
+            String updText = firstPart + clipboardText + lastPart;
+
+            if (clipboardText != null) {
+                tArea.setText(updText);
+                tArea.positionCaret(range.getStart() + clipboardText.length());
+                System.out.println(range.getStart() + clipboardText.length());
+            }
+        });
+        paste.setAccelerator(new KeyCodeCombination(KeyCode.V, KeyCombination.CONTROL_DOWN));
+
+        delete.setOnAction(event -> {
+            IndexRange range = tArea.getSelection();
+
+            String origText = tArea.getText();
+            String firstPart = origText.substring(0, range.getStart());
+            String lastPart = origText.substring(range.getEnd());
+
+            String updText = firstPart + lastPart;
+
+            tArea.setText(updText);
+            tArea.positionCaret(range.getStart());
+        });
+        delete.setAccelerator(new KeyCodeCombination(KeyCode.DELETE));
+
+        edit.getItems().addAll(cut, copy, paste, delete);
+
         MenuButton help = new MenuButton("Help");
         help.setStyle("-fx-focus-color: transparent;");
         MenuItem viewHelp = new MenuItem("View Help");
-
+        viewHelp.setOnAction(event ->  {
+            try {
+                Desktop.getDesktop().browse(new URI("https://lmgtfy.com/?q=download+microsoft+vscode"));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        });
         help.getItems().addAll(viewHelp);
 
-        topButtons.getChildren().addAll(file, help);
+        topButtons.getChildren().addAll(file, edit, help);
 
         vbox.getChildren().addAll(topButtons, tArea);
         VBox.setVgrow(tArea, Priority.ALWAYS);
